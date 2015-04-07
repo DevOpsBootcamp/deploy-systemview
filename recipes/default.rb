d2f03c61fed4af53e2bc55d2d44c3e3ae7ff0e29
Ian Kronquist <iankronquist@teleport.com>
2015-04-06 18:17:47 -0700

    Install website with git and python dependencies

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Include the chef resources we will be using.
+# We will use the git resource to clone the cookbook.
+# We will use the pip resource to install python dependencies.
+# we will use the virtualenv resource to keep all of our python dependencies
+# in one place.
+include_recipe 'git'
+include_recipe 'python::pip'
+include_recipe 'python::virtualenv'
+include_recipe 'supervisor'
+include_recipe 'nginx'
+
+# create the virtual environment for the application using the
+# python_virtualenv resource.
+python_virtualenv node['systemview']['virtualenv_path'] do
+  action :create
+end
+
+# use the git resource to checkout or update the code from the webapp's
+# git repo
+# docs on the git resource can be found here:
+# https://docs.chef.io/resource_git.html
+git node['systemview']['app_path'] do
+  repository node['systemview']['repository']
+  revision node['systemview']['revision']
+  action :sync
+end
+
+# install the python packages which systemview needs using the requirements.txt
+# file. this is equivalent to pip install -r /opt/systemview/requirements.txt
+python_pip "#{ node['systemview']['app_path'] }/requirements.txt" do
+  action :install
+  options '-r'
+  virtualenv node['systemview']['virtualenv_path']
+end
